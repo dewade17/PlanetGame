5b83fabd3fd12c38ad23cfcb21d46a28be5b3559
Dewade17 <92839006+dewade17@users.noreply.github.com>
2021-10-20 11:39:49 +0800

    updated

---
+++ timerpromosi.js
@@ -0,0 +1,25 @@
+var countDate = new Date('oct 31 2021 00:00:00').getTime();
+
+function promosi() {
+  var now = new Date().getTime();
+  gap = countDate - now;
+
+  var Detik = 1000;
+  var Menit = Detik * 60;
+  var Jam = Menit * 60;
+  var Hari = Jam * 24;
+
+  var h = Math.floor(gap / Hari);
+  var j = Math.floor((gap % Hari) / Jam);
+  var m = Math.floor((gap % Jam) / Menit);
+  var d = Math.floor((gap % Menit) / Detik);
+
+  document.getElementById('Hari').innerText = h;
+  document.getElementById('Jam').innerText = j;
+  document.getElementById('Menit').innerText = m;
+  document.getElementById('Detik').innerText = d;
+}
+
+setInterval(function () {
+  promosi();
+}, 1000);
