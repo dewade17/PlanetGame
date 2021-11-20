3f8dd45dc1efec50b23a49a554ba1daf2c5d7b14
dewade17 <andrenewer@gmail.com>
2021-11-20 21:53:08 +0800

    Update timerpromosi.js

--- timerpromosi.js
+++ timerpromosi.js
@@ -1,4 +1,4 @@
-var countDate = new Date('nov 30 2021 00:00:00').getTime();
+var countDate = new Date('Nov 30 2021 00:00:00').getTime();
 
 function promosi() {
   var now = new Date().getTime();
@@ -21,10 +21,10 @@ function promosi() {
 
   if (gap < 1144) {
     clearInterval(gap);
-    document.getElementById('Hari').innerText = '00';
-    document.getElementById('Jam').innerText = '00';
-    document.getElementById('Menit').innerText = '00';
-    document.getElementById('Detik').innerText = '00';
+    document.getElementById('Hari').innerText = '0';
+    document.getElementById('Jam').innerText = '0';
+    document.getElementById('Menit').innerText = '0';
+    document.getElementById('Detik').innerText = '0';
   }
 }
 
